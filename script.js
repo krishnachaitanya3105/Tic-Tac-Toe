56da9ab4e87522277d7bc3944abfbea8c3bf87c5
krishnachaitanya3105 <krishnachaitanya31052006@gmail.com>
2024-10-20 10:38:00 +0530

    logic is completed . now u will get output on console not on the screen need to update

--- script.js
+++ script.js
@@ -32,6 +32,31 @@ checkwinner();
 
 const checkwinner = () => {
     for (let pattern of Object.values(winpatterns) ){
-        console.log(pattern); 
+         let pos1val= boxes[pattern[0]].innerText;
+         let pos2val=   boxes[pattern[1]].innerText;
+         let pos3val=  boxes[pattern[2]].innerText;
+
+         if(pos1val!="" && pos2val!="" && pos3val!="")
+         {
+            if(pos1val===pos2val && pos2val===pos3val)
+            {
+                console.log("Winner",pos1val);
+            }
+         }
+        }
+     
     }
-}
+
+
+
+
+
+
+
+
+
+let select=document.getElementById("select");
+select.addEventListener("change",()=>{
+let selectedcolor=select.value;
+document.body.style.backgroundColor=selectedcolor;
+})
